1b04a4c7d417fd5c26cfdfea5d7b8ae8077e409b
Programm0 <1Pro2gra3mm45@web.de>
2021-10-25 14:35:33 +0200

    Cleaning up main and try download

--- scripty-app/src/main/java/com/programm/projects/scripty/app/ScriptyApp.java
+++ scripty-app/src/main/java/com/programm/projects/scripty/app/ScriptyApp.java
@@ -1,48 +1,11 @@
 package com.programm.projects.scripty.app;
 
-import com.programm.projects.scripty.module.api.SyContext;
-import com.programm.projects.scripty.module.api.io.IInput;
-import com.programm.projects.scripty.module.api.io.IOutput;
+import java.util.Arrays;
 
-import java.util.HashMap;
-import java.util.Map;
+public class ScriptyApp {
 
-public class ScriptyApp implements SyContext {
-
-    private static final Map<Class<?>, Object> getMap = new HashMap<>();
-    private static final ScriptyApp APP = new ScriptyApp();
-
-    static {
-        getMap.put(SyContext.class, APP);
+    public static void main(String[] args) {
+        System.out.println("ARGS: " + Arrays.toString(args));
     }
 
-    public static void main(String[] args) throws Exception{
-        ExecutableModule res = ModuleBuilder.buildModule(TestModule.class, ScriptyApp::getFunc);
-        ModuleBuilder.setupModule(res.module, APP);
-        System.out.println("");
-    }
-
-    private static Object getFunc(Class<?> cls){
-        return getMap.get(cls);
-    }
-
-    @Override
-    public IOutput log() {
-        return null;
-    }
-
-    @Override
-    public IOutput out() {
-        return null;
-    }
-
-    @Override
-    public IOutput err() {
-        return null;
-    }
-
-    @Override
-    public IInput in() {
-        return null;
-    }
 }
